dfeea67772477688a18e341a523c9d0d0ff4513a
Sal Tamay <sal.tamay@outlook.com>
2019-12-30 20:01:39 -0500

    feat: Update Artist model with migrations

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ class Venue(db.Model):
     seeking_description = db.Column(db.String)
     image_link = db.Column(db.String(500))
     past_shows_count = db.Column(db.Integer)
-    upcoming_shows_count = db.Column(db.Integer, nullable=False, default=0)
+    upcoming_shows_count = db.Column(db.Integer)
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
@@ -56,12 +56,18 @@ class Artist(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
+    genres = db.Column(db.ARRAY(db.String))
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
+    website = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String)
+    image_link = db.Column(db.String(500))
+    past_shows_count = db.Column(db.Integer)
+    upcoming_shows_count = db.Column(db.Integer)
+    
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
